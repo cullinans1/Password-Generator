d678913c5f357f088cd971e740ee727c5a2834e9
cullinans1  <sammy.cullinan@gmail.com>
2020-06-14 14:30:29 -0700

    added random if true then find random char value to each confirm

--- Develop/script.js
+++ Develop/script.js
@@ -103,58 +103,87 @@ var numbers = [
   "8",
   "9"
 ];
-function getRandomLower() {
- return lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
-}
-console.log(getRandomLower());
-
-function getRandomUpper() {
-  return upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
-}
-console.log(getRandomUpper());
 var passwordOptions = function () {
   //selecting length, parseInt to have input change to integer
-    var passLength = parseInt (
-      prompt("How many characters would you like your password to be? (Select a number 8-128)")
-      );
-      //If invalid number return to question
-      if (passLength <= 7 || passLength > 128) {
-        alert("Pick a value between 8 - 128!");
-        return;
-      } 
-      //Return if entry is NaN (Not a number)
-      if(isNaN(passLength) === true) {
-        alert("Pick a number that is between 8 - 128!");
-        return;
-      }
-    //Confirmation for special characters
-    var confirmSpecialChar = confirm( 
-      "Would you like your password to have special characters?"
+  var passLength = parseInt (
+    prompt("How many characters would you like your password to be? (Select a number 8-128)")
     );
-      if(confirmSpecialChar === true) {
+    //If invalid number return to question
+    if (passLength <= 7 || passLength > 128) {
+      alert("Pick a value between 8 - 128!");
+      return;
+    } 
+    //Return if entry is NaN (Not a number)
+    if(isNaN(passLength) === true) {
+      alert("Pick a number that is between 8 - 128!");
+      return;
+    }
+  //Confirmation for special characters
+  var confirmSpecialChar = confirm( 
+    "Would you like your password to have special characters?"
+  );
+  //Confirmation for lowercase
+  var confirmLowerCase = confirm (
+    "Would you like your password to have lower case letters?"
+  );
+  //Confirmation for uppercase
+  var confirmUpperCase = confirm (
+    "Woud you like your password to have upper case letters?"
+  );
+  //Confirmation for numbers
+  var confirmNum = confirm (
+  "Would you like your password to have numbers?"
+  );
 
-      } else {
-        
-      }
-    //Confirmation for lowercase
-    var confirmLowerCase = confirm (
-      "Would you like your password to have lower case letters?"
-    );
-    //Confirmation for uppercase
-    var confirmUpperCase = confirm (
-      "Woud you like your password to have upper case letters?"
-    );
-    //Confirmation for numbers
-    var confirmNum = confirm (
-      "Would you like your password to have numbers?"
-    );
-//funtion to create a random number and that is how many it will grab from each array with a max of password length
-};
-//created variable to create a running list of characters that will be in password
-var runningList = [];
+  //creating an obj to store the input
+  var passwordInput = {
+    passLength: passLength,
+    confirmSpecialChar: confirmSpecialChar,
+    confirmLowerCase: confirmLowerCase,
+    confirmUpperCase: confirmUpperCase,
+    confirmNum: confirmNum
+  };
+return passwordInput;
+}
 
+//function to create password
+function generatePassword() {
+  //function to pull what the user input
+  var userOptions = passwordOptions();
+  
+  //variable to store password
+  var passwordFinal = [];
+  
+  //variable to store random values 
+  var rndmValues = [];
 
-passwordOptions();
+  //pull random characters from each array if its true
+  if(userOptions.confirmSpecialChar) {
+    function randomSpC() {
+      return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    };
+    console.log(randomSpC());
+  }
+  if(userOptions.confirmLowerCase) {
+    function randomLower() {
+      return lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
+    };
+    console.log(randomLower());
+  }
+  if(userOptions.confirmUpperCase) {
+    function randomUpper() {
+      return upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
+    };
+    console.log(randomUpper());
+  }
+  if(userOptions.confirmNum) {
+    function randomNum() {
+      return numbers[Math.floor(Math.random() * numbers.length)];
+    };
+    console.log(randomNum());
+  }
+}
+generatePassword();
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
