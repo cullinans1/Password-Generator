eab5188b84e1a8f573ff0f55069069777e00f558
cullinans1  <sammy.cullinan@gmail.com>
2020-06-14 11:45:07 -0700

    testing for loop to generate password

--- Develop/script.js
+++ Develop/script.js
@@ -103,15 +103,34 @@ var numbers = [
   "8",
   "9"
 ];
+const randomPass = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNum,
+  special: getRandomSpecial,
+}
+//function to get random lowercase letter out of array
 function getRandomLower() {
  return lowerCaseLetters[Math.floor(Math.random() * lowerCaseLetters.length)];
 }
 console.log(getRandomLower());
-
+//function to get random uppercase letter out of its array
 function getRandomUpper() {
   return upperCaseLetters[Math.floor(Math.random() * upperCaseLetters.length)];
 }
 console.log(getRandomUpper());
+//function to get a random number out of its array
+function getRandomNum() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+console.log(getRandomNum());
+//function to get a random special character out of its array 
+function getRandomSpecial() {
+  return specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+}
+console.log(getRandomSpecial());
+
+//prompt/confirms for password selection
 var passwordOptions = function () {
   //selecting length, parseInt to have input change to integer
     var passLength = parseInt (
@@ -131,11 +150,6 @@ var passwordOptions = function () {
     var confirmSpecialChar = confirm( 
       "Would you like your password to have special characters?"
     );
-      if(confirmSpecialChar === true) {
-
-      } else {
-        
-      }
     //Confirmation for lowercase
     var confirmLowerCase = confirm (
       "Would you like your password to have lower case letters?"
@@ -148,13 +162,46 @@ var passwordOptions = function () {
     var confirmNum = confirm (
       "Would you like your password to have numbers?"
     );
-//funtion to create a random number and that is how many it will grab from each array with a max of password length
+    //making the user input save as its value
+  var passwordSelections = {
+    passLength: passLength,
+    confirmSpecialChar: confirmSpecialChar,
+    confirmLowerCase: confirmLowerCase,
+    confirmUpperCase: confirmUpperCase,
+    confirmNum: confirmNum
+  };
+  //function to generate password based off of input
+  var generatePassword = function () {
+    //initalize Password variable
+    // filter out user input (take out false values)
+    //loop over length call generate function for each type
+    // add final password to display and return
+    // empty variable for password values to be put into 
+    var finalPassword = ""; 
+
+    var types = confirmSpecialChar + confirmLowerCase + confirmNum + confirmUpperCase;
+
+    var typesArray = [{confirmSpecialChar}, {confirmLowerCase}, {confirmNum}, {confirmUpperCase}].filter(item => Object.values(item) [0] );
+    console.log(typesArray);
+    if(typesArray === 0) {
+      alert("Your password will need to contain something! Please press OK on at least one of the options.");
+      return;
+    }
+    for(i = 0; i < length; i += types) {
+      typesArray.forEach(type => {
+        var funct = Object.keys(type)[0];
+        console.log("password", passwordSelections)
+
+        finalPassword += passwordSelections[funct]();
+      });
+    }
+  };
+  generatePassword();
+  //generatePassword (passwordSelections);
 };
-//created variable to create a running list of characters that will be in password
-var runningList = [];
-
-
 passwordOptions();
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
